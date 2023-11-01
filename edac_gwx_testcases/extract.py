ebc95ab7c5dd0d9c8203924851612ca9265e09e0
NeverLuck2001 <53634857+NeverLuck2001@users.noreply.github.com>
2023-11-01 15:38:16 +0800

    变更

--- edac_gwx_testcases/extract.py
+++ edac_gwx_testcases/extract.py
@@ -1,68 +1,101 @@
 import numpy as np
 import os
 import re
-from rename import rename1, rename2
+from rename import rename1, rename2, rename3
 
-path = './testcase03/'
-np_path = path + 'ds_np/'
-ds_path = path + 'ds/'
-if not os.path.exists(np_path):
-    os.makedirs(np_path)
+path0 = './testcase0'
+for q in range(10):
+    if os.path.exists(path0 + str(q) + '/'):
+        path = path0 + str(q) + '/'
+        np_path = path + 'ds_np/'
+        ds_path = path + 'ds/'
+        if not os.path.exists(np_path):
+            os.makedirs(np_path)
+        # np.load("D:/PythonProj/edac_gwx_testcases/testcase03/ds_np/spsram_256x12m2s_tt1v25c.txt.npy")
+        list1 = os.listdir(ds_path)
+        length1 = len(list1)
 
-#np.load("D:/PythonProj/edac_gwx_testcases/testcase03/ds_np/spsram_256x12m2s_tt1v25c.txt.npy")
+        if list1[0][-2:] == 'ds':
+            rename1(ds_path, 'ds')
+            list2 = os.listdir(ds_path)
+            for i in range(length1):
+                print(list2[i])
+                f = open(ds_path + list2[i], 'r')  # f = open('spsram_64x63m4s_tt1v25c.txt','r')
+                arr = np.array([])[:, np.newaxis]
 
-list1 = os.listdir(ds_path)
-rename1(ds_path,'ds')
-list2 = os.listdir(ds_path)
-length1 = len(list1)
+                for line in f:
+                    if "Width" in line and "Area" in line:
+                        line = f.readline()
+                        line = f.readline()
+                        line = re.findall(r"\d+\.?\d*", line)
+                        f3 = np.expand_dims(np.array(float(line[-1])), axis=0)[:, np.newaxis]
+                        f2 = np.expand_dims(np.array(float(line[-2])), axis=0)[:, np.newaxis]
+                        f1 = np.expand_dims(np.array(float(line[-3])), axis=0)[:, np.newaxis]
+                        arr = np.concatenate((arr, f1, f2, f3), axis=0)
+                        print('Area')
+                        print(arr)
+                    if "Leakage Current" in line and "uA" in line:
+                        line = re.findall(r"\d+\.?\d*", line)
+                        f3 = np.expand_dims(np.array(float(line[2])), axis=0)[:, np.newaxis]
+                        f2 = np.expand_dims(np.array(float(line[1])), axis=0)[:, np.newaxis]
+                        f1 = np.expand_dims(np.array(float(line[0])), axis=0)[:, np.newaxis]
+                        arr = np.concatenate((arr, f1, f2, f3), axis=0)
+                        print('Leakage Current')
+                        print(arr)
+                    if "Factor" in line and "Value" in line:
+                        line = f.readline()
+                        for p in range(7):
+                            line = f.readline()
+                            line = re.findall(r"\d+\.?\d*", line)
+                            f1 = np.expand_dims(np.array(float(line[-1])), axis=0)[:, np.newaxis]
+                            arr = np.concatenate((arr, f1), axis=0)
+                        print('Factor')
+                        print(arr)
+                    if "Pins" in line and "VDD" in line:
+                        line = f.readline()
+                        line = f.readline()
+                        for p in range(13):
+                            line = f.readline()
+                            line = re.findall(r"\d+\.?\d*", line)
+                            f1 = np.expand_dims(np.array(float(line[-1])), axis=0)[:, np.newaxis]
+                            arr = np.concatenate((arr, f1), axis=0)
+                        print('Pins')
+                        print(arr)
+                f.close()
+                np.save(np_path + list1[i], arr)
 
-for i in range(length1):
-    print(list2[i])
-    f = open(ds_path+list2[i],'r')  # f = open('spsram_64x63m4s_tt1v25c.txt','r')
-    arr = np.array([])[:,np.newaxis]
+            rename2(ds_path, 'txt')
 
-    for line in f :
-        if "Width" in line and "Area" in line:
-            line = f.readline()
-            line = f.readline()
-            line = re.findall(r"\d+\.?\d*", line)
-            f3 = np.expand_dims(np.array(float(line[-1])), axis=0)[:,np.newaxis]
-            f2 = np.expand_dims(np.array(float(line[-2])), axis=0)[:,np.newaxis]
-            f1 = np.expand_dims(np.array(float(line[-3])), axis=0)[:,np.newaxis]
-            arr = np.concatenate((arr,f1,f2,f3),axis=0)
-            print('Area')
-            print(arr)
-        if "Leakage Current" in line and "uA" in line:
-            line = re.findall(r"\d+\.?\d*",line)
-            f3 = np.expand_dims(np.array(float(line[2])), axis=0)[:,np.newaxis]
-            f2 = np.expand_dims(np.array(float(line[1])), axis=0)[:,np.newaxis]
-            f1 = np.expand_dims(np.array(float(line[0])), axis=0)[:,np.newaxis]
-            arr = np.concatenate((arr,f1,f2,f3),axis=0)
-            print('Leakage Current')
-            print(arr)
-        if "Factor" in line and "Value" in line:
-            line = f.readline()
-            for p in range(7):
-                line = f.readline()
-                line = re.findall(r"\d+\.?\d*",line)
-                f1 = np.expand_dims(np.array(float(line[-1])), axis=0)[:,np.newaxis]
-                arr = np.concatenate((arr,f1),axis=0)
-            print('Factor')
-            print(arr)
-        if "Pins" in line and "VDD" in line:
-            line = f.readline()
-            line = f.readline()
-            for p in range(13):
-                line = f.readline()
-                line = re.findall(r"\d+\.?\d*",line)
-                f1 = np.expand_dims(np.array(float(line[-1])), axis=0)[:,np.newaxis]
-                arr = np.concatenate((arr,f1),axis=0)
-            print('Pins')
-            print(arr)
-    f.close()
-    np.save(np_path+list2[i],arr)
+        elif list1[0][-4:] == 'summ':
+            rename1(ds_path, 'summ')
+            list2 = os.listdir(ds_path)
+            for i in range(length1):
+                print(list2[i])
+                f = open(ds_path + list2[i], 'r')  # np.load('ram1kx32_2p1r1w.summ.npy')
+                arr = np.array([])[:, np.newaxis]
+                for line in f:
+                    if "CONFIGURATION" in line and "Power" in line:
+                        line = f.readline()
+                        line = re.findall(r"\d+\.?\d*", line)
+                        f2 = np.expand_dims(np.array(float(line[-1])), axis=0)[:, np.newaxis]
+                        f1 = np.expand_dims(np.array(float(line[-2])), axis=0)[:, np.newaxis]
+                        arr = np.concatenate((arr, f1, f2), axis=0)
+                        print('CONFIGURATION')
+                        print(arr)
+                    if "Voltage" in line and "Temperature" in line:
+                        line = f.readline()
+                        line = re.findall(r"\d+\.?\d*", line)
+                        f4 = np.expand_dims(np.array(float(line[3])), axis=0)[:, np.newaxis]
+                        f3 = np.expand_dims(np.array(float(line[2])), axis=0)[:, np.newaxis]
+                        f2 = np.expand_dims(np.array(float(line[1])), axis=0)[:, np.newaxis]
+                        f1 = np.expand_dims(np.array(float(line[0])), axis=0)[:, np.newaxis]
+                        arr = np.concatenate((arr, f1, f2, f3, f4), axis=0)
+                        print('Voltage')
+                        print(arr)
+                f.close()
+                np.save(np_path + list1[i], arr)
 
-rename2(ds_path,'txt')
+            rename3(ds_path, 'txt')
 
-
-
+    else:
+        continue
--- edac_gwx_testcases/rename.py
+++ edac_gwx_testcases/rename.py
@@ -37,3 +37,22 @@ def rename2(path,origin):
             os.rename(oldname, newname)
 
             print(oldname, '======>', newname)
+
+def rename3(path,origin):
+#获取该目录下所有文件，存入列表中
+    f = os.listdir(path)
+    length0 = len(origin)
+    for i in f:
+
+        if i[-length0:] == origin:
+
+            #设置旧文件名（就是路径+文件名）
+            oldname = path + i
+
+            #设置新文件名
+            newname = path + i[:-length0] + 'summ'
+
+            #用os模块中的rename方法对文件改名
+            os.rename(oldname, newname)
+
+            print(oldname, '======>', newname)
