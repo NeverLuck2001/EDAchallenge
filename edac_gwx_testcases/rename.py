603ffc63a8af525e93e440e08d7ca6d5aeca402d
NeverLuck2001 <53634857+NeverLuck2001@users.noreply.github.com>
2023-11-06 05:23:32 +0800

    变更

--- edac_gwx_testcases/rename.py
+++ edac_gwx_testcases/rename.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def rename1(path,origin):
 #获取该目录下所有文件，存入列表中
@@ -56,3 +57,52 @@ def rename3(path,origin):
             os.rename(oldname, newname)
 
             print(oldname, '======>', newname)
+
+
+def rename4(path, origin):
+    # 获取该目录下所有文件，存入列表中
+    f = os.listdir(path)
+    length0 = len(origin)
+    for i in f:
+
+        if i[-length0:] == origin:
+            # 设置旧文件名（就是路径+文件名）
+            oldname = path + i
+
+            # 设置新文件名
+            newname = path + i[:-length0] + 'lib'
+
+            # 用os模块中的rename方法对文件改名
+            os.rename(oldname, newname)
+
+            print(oldname, '======>', newname)
+
+def rename5(lvlib_path, key, key1):
+    # 获取该目录下所有文件，存入列表中
+    f = os.listdir(lvlib_path)
+
+    for i in f:
+
+            # 设置旧文件名（就是路径+文件名）
+            oldname = lvlib_path + i
+
+            # 设置新文件名
+            newname = oldname.replace(key1,key)
+
+            # 用os模块中的rename方法对文件改名
+            os.rename(oldname, newname)
+
+
+def rename6(lvlib_path, key, key1):
+    # 获取该目录下所有文件，存入列表中
+    f = os.listdir(lvlib_path)
+
+    for i in f:
+        # 设置旧文件名（就是路径+文件名）
+        oldname = lvlib_path + i
+
+        # 设置新文件名
+        newname = oldname.replace(key, key1)
+
+        # 用os模块中的rename方法对文件改名
+        os.rename(oldname, newname)
